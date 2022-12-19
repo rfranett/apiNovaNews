f9568fc78a9e5b040569a65942a9ba55b39d1315
rfranett <rfranett@gmail.com>
2022-12-19 18:55:59 -0300

    porta 3000 nao ativa

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-app.get('/', (req, res) =>{
-  res.send('Hello World porra');
+//rota
+// method http -crud = (create,read,update,delete)
+// GET -pega uma info
+// POST - cria uma info
+// PUT - altera parte da info
+// DELETE - apaga uma info
+// name
+// function (callback)
+
+
+app.get("/soma", (req, res) => {
+  const soma = 1+ 1 ;
+
+  res.send(soma)
 });
 
 app.listen(3000);
